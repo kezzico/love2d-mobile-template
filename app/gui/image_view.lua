01ff8edcc6ddb23e43d175c14a426cdf3083474f
Lee Irvine <lee@kezzi.co>
2026-06-03 10:58:21 -0700

    add stretch type to image_view

--- app/gui/image_view.lua
+++ app/gui/image_view.lua
@@ -9,24 +9,30 @@ function ImageView(path, style)
 
 		draw = function(self, w, h)
 			local sx = 1
+			local sy = 1
 			if style.aspect == "fill" then
 				sx = math.max(w / self.native_width, h / self.native_height)
+				sy = sx
 			elseif style.aspect == "fit" then
 				sx = math.min(w / self.native_width, h / self.native_height)
+				sy = sx
+			elseif style.aspect == "stretch" then
+				sx = w / self.native_width
+				sy = h / self.native_height
 			end
 
 			local center_x = (w * 0.5) - (sx * self.native_width * 0.5)
-			local center_y = (h * 0.5) - (sx * self.native_height * 0.5)
+			local center_y = (h * 0.5) - (sy * self.native_height * 0.5)
 			local ox = center_x
 			local oy = 0
 
 			if style.align == "center" then
 				oy = center_x
 			elseif style.align == "bottom" then
-				oy = h - (self.native_height * sx)
+				oy = h - (self.native_height * sy)
 			end
 
-      love.graphics.draw(image, ox, oy, 0, sx, sx)
+			love.graphics.draw(image, ox, oy, 0, sx, sy)
 		end,
 	}
 
