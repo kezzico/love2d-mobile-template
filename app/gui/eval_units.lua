eea8b3066dd7f841eee7ad5969a587c62f378068
Lee Irvine <lee@kezzi.co>
2026-07-30 17:33:53 -0700

    eval_units support for addition/subtraction
    example: "100%-22px"

--- app/gui/eval_units.lua
+++ app/gui/eval_units.lua
@@ -1,6 +1,15 @@
 local function eval_units(value_plus_unit, relative_to)
   local r = value_plus_unit
 
+  if type(r) == "string" and (r:find("[+-]", 2)) then
+    local val1, val2 = r:match("^([^+-]+)([+-].+)$")
+
+    local a = eval_units(val1, relative_to)
+    local b = eval_units(val2, relative_to)
+    
+    return a + b
+  end
+
   if type(r) == "string" and r:sub(-1) == "%" then
     r = tonumber(r:sub(1, -2)) * relative_to / 100
   elseif type(r) == "string" and r:sub(-2) == "px" then
